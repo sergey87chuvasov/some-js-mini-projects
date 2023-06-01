386a743b87f8f8ed4af9c70a1fd219797ab75744
Sergey Chv <serge.techno@gmail.com>
2023-06-02 00:56:38 +0300

    added mine js project - animated pop up box

---
+++ PopUpBox/script.js
@@ -0,0 +1,9 @@
+const popup = document.getElementById('popup');
+
+function openSlide() {
+    popup.classList.add('open-slide');
+}
+
+function closeSlide() {
+    popup.classList.remove('open-slide');
+}
