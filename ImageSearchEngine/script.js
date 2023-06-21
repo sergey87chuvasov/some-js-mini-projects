4b3f6241db0f16943d1097d8ca8576cdf402efc4
Sergey Chv <serge.techno@gmail.com>
2023-06-21 21:53:59 +0300

    added mini js project - image search engine - 1 part

---
+++ ImageSearchEngine/script.js
@@ -0,0 +1,10 @@
+const searchForm = document.getElementById('search-form');
+const searchBox = document.getElementById('search-box');
+const searchResult = document.getElementById('search-result');
+const showMoreBtn = document.getElementById('show-more-btn');
+
+
+
+https://api.unsplash.com/search/collections?page=1&query=office>; rel="first", <https://api.unsplash.com/search/collections?page=1&query=office>; rel="prev", <https://api.unsplash.com/search/collections?page=3&query=office>; rel="last", 
+<https://api.unsplash.com/search/collections?page=3&query=office>; rel="next"
+4cpd-wZSXNb5kUqrOqjO8Tbdrmqo0wK4kQf45Euzgt0
