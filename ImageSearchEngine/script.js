972e1e1f89077f5b27acb5e806b62cc8d3de135c
Sergey Chv <serge.techno@gmail.com>
2023-06-21 23:30:34 +0300

    added mini js project - image search engine - 2 part - finished project

--- ImageSearchEngine/script.js
+++ ImageSearchEngine/script.js
@@ -1,10 +1,51 @@
+const accessKey = '4cpd-wZSXNb5kUqrOqjO8Tbdrmqo0wK4kQf45Euzgt0';
+
 const searchForm = document.getElementById('search-form');
 const searchBox = document.getElementById('search-box');
 const searchResult = document.getElementById('search-result');
 const showMoreBtn = document.getElementById('show-more-btn');
 
+let keyword = '';
+let page = 1;
 
+async function searchImages() {
+    keyword = searchBox.value;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`;
 
-https://api.unsplash.com/search/collections?page=1&query=office>; rel="first", <https://api.unsplash.com/search/collections?page=1&query=office>; rel="prev", <https://api.unsplash.com/search/collections?page=3&query=office>; rel="last", 
-<https://api.unsplash.com/search/collections?page=3&query=office>; rel="next"
-4cpd-wZSXNb5kUqrOqjO8Tbdrmqo0wK4kQf45Euzgt0
+    const response = await fetch(url);
+
+    const data = await response.json();
+
+    //check
+    if(page === 1) {
+        searchResult.innerHTML = '';
+    }
+
+    // console.log(data);
+    const results = data.results;
+
+    results.map((result) => {
+       const image = document.createElement('img');
+       image.src = result.urls.small; 
+
+       const imageLink = document.createElement('a');
+       imageLink.href = result.links.html;
+       imageLink.target = '_blank';
+
+       imageLink.appendChild(image); // inside img
+       searchResult.appendChild(imageLink);
+    })
+
+    showMoreBtn.style.display = 'block';
+}
+
+searchForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    page = 1;
+    searchImages();
+})
+
+showMoreBtn.addEventListener('click', () => {
+    page++;
+    searchImages();
+})
