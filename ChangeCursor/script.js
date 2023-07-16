cc01057f301ed4d4f3a6930a92ab65f5602f374e
Sergey Chv <serge.techno@gmail.com>
2023-07-16 16:26:14 +0300

    added mini js project - Change Cursor on Hover

---
+++ ChangeCursor/script.js
@@ -0,0 +1,6 @@
+let cursor = document.querySelector('.cursor');
+
+document.addEventListener('mousemove', (e => {
+    cursor.style.left = e.pageX + 'px';
+    cursor.style.top = e.pageY + 'px';
+}))
